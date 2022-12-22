66744fef47754e6fcd07285d1b3ed467193f6766
williamxmejia <willx1995@gmail.com>
2022-12-21 21:37:51 -0600

    added the state and the hooks, displaying info on screen. Now its time to redesign the app

--- chainfolio/src/components/HomePage.js
+++ chainfolio/src/components/HomePage.js
@@ -5,30 +5,40 @@ import Navbar from "./Navbar";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
 // dotenv.config()
 
 const HomePage = () => {
+  const[data, setData] = useState({
+    name:"",
+    symbol: "",
+    cmc_rank: "",
+    circulating_supply: "",
+    total_supply: "",
+    max_supply: "",
+    self_reported_market_cap:"" 
+  })
 
-    // conts[data, setData] = useState()
 
-    useEffect(() => {
-        async function fetchData() {
-            await axios("http://localhost:5000/", {
-                method: "GET",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              }).then(res => {
-                  console.log(res)
-              })
-              .catch((error) => {
-                window.alert(error);
-                return;
-              });
-        }
-        fetchData()
-    }, [])
+  useEffect(() => {
+    async function fetchData() {
+      await axios("http://localhost:5000/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify()
+      })
+        .then((res) => {
+            console.log(res);
+            setData(res.data);
+        })
+        .catch((error) => {
+          window.alert(error);
+          return;
+        });
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="container">
@@ -37,6 +47,15 @@ const HomePage = () => {
         <div className="d-flex">
           <div className="mx-5 mt-5 col-7 text-white">
             <h2 className="text-center">Welcome to ChainFolio</h2>
+            <h1>{data.symbol}</h1>
+            <h2>{data.name}</h2>
+            <ul>
+                <li>{data.cmc_rank}</li>
+                <li>{data.self_reported_market_cap}</li>
+                <li>{data.circulating_supply} {data.symbol}</li>
+                <li>{data.total_supply} {data.symbol}</li>
+                <li>{data.max_supply} {data.symbol}</li>
+            </ul>
             <p
               className="mt-5 text-center"
               style={{ fontWeight: "400", fontSize: "25px" }}
